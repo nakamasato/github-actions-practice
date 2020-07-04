155b76592eb02f7437bac3aae8ede49254cac0ac
masatonaka <masatonaka1989@gmail.com>
2020-07-04 15:25:16 +0900

    use SOURCE_BRANCH

--- .github/actions/check-change.sh
+++ .github/actions/check-change.sh
@@ -2,8 +2,8 @@
 set -eu
 
 AUTO_MERGE_DIR_REGEX='check-change/.*/auto-merge/.*.yaml'
-git fetch origin master
-NON_AUTOMERGE_FILE_NUM=$(git diff --name-only origin/master HEAD | grep -vE "$AUTO_MERGE_DIR_REGEX")
+git fetch origin "$BASE_BRANCH"
+NON_AUTOMERGE_FILE_NUM=$(git diff --name-only "origin/$BASE_BRANCH" HEAD | grep -vE "$AUTO_MERGE_DIR_REGEX")
 PR_COMMENT_CONTENT_TMP_FILE=comment
 if [ "$NON_AUTOMERGE_FILE_NUM" == 0 ];then
     echo "auto-approve" >> $PR_COMMENT_CONTENT_TMP_FILE
@@ -15,6 +15,8 @@ echo "GITHUB_REF: $GITHUB_REF"
 echo "GITHUB_SHA: $GITHUB_SHA"
 echo "SOURCE_BRANCH $SOURCE_BRANCH"
 echo "BASE_BRANCH $BASE_BRANCH"
+echo "GITHUB_REPOSITORY: $GITHUB_REPOSITORY"
+
 echo "auto-approve condition is: \`$AUTO_MERGE_DIR_REGEX\` defined in $GITHUB_REPOSITORY/.github/actions/check-change.sh" >> $PR_COMMENT_CONTENT_TMP_FILE
 sed -i -z 's/\n/\\n/g' $PR_COMMENT_CONTENT_TMP_FILE
 
