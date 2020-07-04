0fe39ddf7a03919fdea104f5ad896826f986572e
masatonaka <masatonaka1989@gmail.com>
2020-07-04 13:17:08 +0900

    add comment

--- .github/actions/check-change.sh
+++ .github/actions/check-change.sh
@@ -5,8 +5,9 @@ AUTO_MERGE_DIR_REGEX='check-change/.*/auto-merge/.*.yaml'
 git fetch origin master
 NON_AUTOMERGE_FILE_NUM=$(git diff --name-only origin/master HEAD | grep -vE $AUTO_MERGE_DIR_REGEX)
 if [[ $NON_AUTOMERGE_FILE_NUM = 0 ]];then
-    echo "AUTO MERGE"
+    echo "Can auto merge" >> comments
 else
-    echo "CANNOT AUTO MERGE as the following files are changed"
-    git diff --name-only origin/master HEAD | grep -vE $AUTO_MERGE_DIR_REGEX
+    echo "Cannot auto merge as the following files are changed:" >> comments
+    git diff --name-only origin/master HEAD | grep -vE $AUTO_MERGE_DIR_REGEX >> comments
+    sed -i -z 's/\n/\\n/g' comments
 fi
