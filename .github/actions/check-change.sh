5f13eb77cad7b549ca64fe0551b44586a36f447a
masatonaka <masatonaka1989@gmail.com>
2020-07-04 13:29:07 +0900

    fix message

--- .github/actions/check-change.sh
+++ .github/actions/check-change.sh
@@ -4,7 +4,7 @@ set -eu
 AUTO_MERGE_DIR_REGEX='check-change/.*/auto-merge/.*.yaml'
 git fetch origin master
 NON_AUTOMERGE_FILE_NUM=$(git diff --name-only origin/master HEAD | grep -vE $AUTO_MERGE_DIR_REGEX)
-if [[ $NON_AUTOMERGE_FILE_NUM = 0 ]];then
+if [ $NON_AUTOMERGE_FILE_NUM = 0 ];then
     echo "auto-approve" >> comments
 else
     echo "auto-approve is skipped as the following files are changed:" >> comments
