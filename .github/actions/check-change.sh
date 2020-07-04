064a2a79d872d7c2dc680c6cf5e09409221e51e6
masatonaka <masatonaka1989@gmail.com>
2020-07-04 16:10:32 +0900

    fix

--- .github/actions/check-change.sh
+++ .github/actions/check-change.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 set -eu
 
-AUTO_MERGE_DIR_REGEX='check-change/.*/auto-merge/.*.yaml'
+AUTO_MERGE_FILE_PATH_REGEX='check-change/.*/auto-merge/.*.yaml'
+AUTO_MERGE_ALLOWED_REGEX='(image)'
 PR_COMMENT_CONTENT_TMP_FILE=comment
 if [ -f $PR_COMMENT_CONTENT_TMP_FILE ]; then rm $PR_COMMENT_CONTENT_TMP_FILE; fi
 BASE_BRANCH=${BASE_BRANCH:-master}
 SOURCE_BRANCH=${SOURCE_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 
 git fetch origin "$BASE_BRANCH"
-NON_AUTOMERGE_FILE_NUM=$(git diff --name-only "origin/$BASE_BRANCH" HEAD | grep -vE "$AUTO_MERGE_DIR_REGEX" | wc -l)
+AUTO_MERGE_NOT_MATCH_FILE_NUM=$(git diff --name-only "origin/$BASE_BRANCH" HEAD | grep -vE "$AUTO_MERGE_FILE_PATH_REGEX" | wc -l)
 
-if [ "$NON_AUTOMERGE_FILE_NUM" == 0 ];then
-    echo "auto-approve" >> $PR_COMMENT_CONTENT_TMP_FILE
+if [ "$AUTO_MERGE_NOT_MATCH_FILE_NUM" == 0 ];then
+    AUTO_MERGE_NOT_MATCH_LINE_NUM=$(git diff origin/master HEAD | grep -vE "$AUTO_MERGE_ALLOWED_REGEX" | wc -l)
+    if [ "$AUTO_MERGE_NOT_MATCH_LINE_NUM" == 0 ];then
+        echo "auto-approve" >> $PR_COMMENT_CONTENT_TMP_FILE
+    else
+        echo "auto-approve is skipped as following lines are changed:" >> $PR_COMMENT_CONTENT_TMP_FILE
+        { echo "\`\`\`"; git diff origin/master HEAD | grep -vE "$AUTO_MERGE_ALLOWED_REGEX"; echo "\`\`\`"; } >> $PR_COMMENT_CONTENT_TMP_FILE
+    fi
 else
     echo "auto-approve is skipped as the following files are changed:" >> $PR_COMMENT_CONTENT_TMP_FILE
-    { echo "\`\`\`"; git diff --name-only origin/master HEAD | grep -vE "$AUTO_MERGE_DIR_REGEX"; echo "\`\`\`"; } >> $PR_COMMENT_CONTENT_TMP_FILE
+    { echo "\`\`\`"; git diff --name-only origin/master HEAD | grep -vE "$AUTO_MERGE_FILE_PATH_REGEX"; echo "\`\`\`"; } >> $PR_COMMENT_CONTENT_TMP_FILE
 fi
 
-echo "auto-approve condition is: \`$AUTO_MERGE_DIR_REGEX\` defined in \`.github/actions/check-change.sh\`" >> $PR_COMMENT_CONTENT_TMP_FILE
+echo "auto-approve condition is: \`$AUTO_MERGE_FILE_PATH_REGEX\` defined in \`.github/actions/check-change.sh\`" >> $PR_COMMENT_CONTENT_TMP_FILE
 sed -i -z 's/\n/\\n/g' $PR_COMMENT_CONTENT_TMP_FILE
 
-export NON_AUTOMERGE_FILE_NUM=$NON_AUTOMERGE_FILE_NUM
+export AUTO_MERGE_NOT_MATCH_FILE_NUM=$AUTO_MERGE_NOT_MATCH_FILE_NUM
 echo "Done"
