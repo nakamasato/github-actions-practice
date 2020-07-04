55f5bba14cf9978ceba07626d896e011b3b79b88
masatonaka <masatonaka1989@gmail.com>
2020-07-04 13:11:23 +0900

    fix

--- .github/actions/check-change.sh
+++ .github/actions/check-change.sh
@@ -1,6 +1,12 @@
-echo "GITHUB_SHA: $GITHUB_SHA"
-echo "GITHUB_REF: $GITHUB_REF"
+#!/bin/bash
+set -eu
+
 AUTO_MERGE_DIR_REGEX='check-change/.*/auto-merge/.*.yaml'
 git fetch origin master
-NON_AUTOMERGE_FILES=$(git diff --name-only origin/master HEAD | grep -vE $AUTO_MERGE_DIR_REGEX)
-echo $NON_AUTOMERGE_FILES
+NON_AUTOMERGE_FILE_NUM=$(git diff --name-only origin/master HEAD | grep -vE $AUTO_MERGE_DIR_REGEX)
+if [ $NON_AUTOMERGE_FILES = 0 ];then
+    echo "AUTO MERGE"
+else
+    echo "CANNOT AUTO MERGE as the following files are changed"
+    git diff --name-only origin/master HEAD | grep -vE $AUTO_MERGE_DIR_REGEX
+fi
