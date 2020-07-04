473f160da8026c607df5f017806a0f3f4efccab1
masatonaka <masatonaka1989@gmail.com>
2020-07-04 13:08:24 +0900

    fix

--- .github/actions/check-change.sh
+++ .github/actions/check-change.sh
@@ -1,4 +1,6 @@
 echo "GITHUB_SHA: $GITHUB_SHA"
 echo "GITHUB_REF: $GITHUB_REF"
+AUTO_MERGE_DIR_REGEX='check-change/.*/auto-merge/.*.yaml'
 git fetch origin master
-git diff --name-only origin/master HEAD
+NON_AUTOMERGE_FILES=$(git diff --name-only origin/master HEAD | grep -vE $AUTO_MERGE_DIR_REGEX)
+echo $NON_AUTOMERGE_FILES
