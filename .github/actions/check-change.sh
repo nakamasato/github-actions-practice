d3d2e079285aac93527ed1550f0b5e9f5f91830c
masatonaka <masatonaka1989@gmail.com>
2020-07-04 16:22:25 +0900

    fix

--- .github/actions/check-change.sh
+++ .github/actions/check-change.sh
@@ -11,20 +11,28 @@ SOURCE_BRANCH=${SOURCE_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 git fetch origin "$BASE_BRANCH"
 AUTO_MERGE_NOT_MATCH_FILE_NUM=$(git diff --name-only "origin/$BASE_BRANCH" HEAD | grep -vE "$AUTO_MERGE_FILE_PATH_REGEX" | wc -l)
 
+file_check=' '
+change_check=' '
 if [ "$AUTO_MERGE_NOT_MATCH_FILE_NUM" == 0 ];then
+    file_check='x'
     AUTO_MERGE_NOT_MATCH_LINE_NUM=$(git diff origin/master HEAD | grep -vE "$AUTO_MERGE_ALLOWED_REGEX" | wc -l)
     if [ "$AUTO_MERGE_NOT_MATCH_LINE_NUM" == 0 ];then
-        echo "auto-approve" >> $PR_COMMENT_CONTENT_TMP_FILE
+        change_check='x'
+        echo "**message:** auto-approved" >> $PR_COMMENT_CONTENT_TMP_FILE
     else
-        echo "auto-approve is skipped as following lines are changed:" >> $PR_COMMENT_CONTENT_TMP_FILE
+        echo "**message:** auto-approve is skipped as following lines are changed:" >> $PR_COMMENT_CONTENT_TMP_FILE
         { echo "\`\`\`"; git diff origin/master HEAD | grep -vE "$AUTO_MERGE_ALLOWED_REGEX"; echo "\`\`\`"; } >> $PR_COMMENT_CONTENT_TMP_FILE
     fi
 else
-    echo "auto-approve is skipped as the following files are changed:" >> $PR_COMMENT_CONTENT_TMP_FILE
+    echo "**message:** auto-approve is skipped as the following files are changed:" >> $PR_COMMENT_CONTENT_TMP_FILE
     { echo "\`\`\`"; git diff --name-only origin/master HEAD | grep -vE "$AUTO_MERGE_FILE_PATH_REGEX"; echo "\`\`\`"; } >> $PR_COMMENT_CONTENT_TMP_FILE
 fi
 
-echo "auto-approve condition is: \`$AUTO_MERGE_FILE_PATH_REGEX\` defined in \`.github/actions/check-change.sh\`" >> $PR_COMMENT_CONTENT_TMP_FILE
+echo "
+auto-approve condition (defined in \`.github/actions/check-change.sh\`) is:
+- [$file_check] files: \`$AUTO_MERGE_FILE_PATH_REGEX\`
+- [$change_check] changes: \`$AUTO_MERGE_ALLOWED_REGEX\`
+" >> $PR_COMMENT_CONTENT_TMP_FILE
 sed -i -z 's/\n/\\n/g' $PR_COMMENT_CONTENT_TMP_FILE
 
 export AUTO_MERGE_NOT_MATCH_FILE_NUM=$AUTO_MERGE_NOT_MATCH_FILE_NUM
