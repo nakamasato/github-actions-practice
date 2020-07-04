1fdcbc35bfbca8f2ea6a9023eaedf488e8ad33af
masatonaka <masatonaka1989@gmail.com>
2020-07-04 14:23:29 +0900

    fix shell

--- .github/actions/check-change.sh
+++ .github/actions/check-change.sh
@@ -4,14 +4,15 @@ set -eu
 AUTO_MERGE_DIR_REGEX='check-change/.*/auto-merge/.*.yaml'
 git fetch origin master
 NON_AUTOMERGE_FILE_NUM=$(git diff --name-only origin/master HEAD | grep -vE "$AUTO_MERGE_DIR_REGEX")
+PR_COMMENT_CONTENT_TMP_FILE=comment
 if [ "$NON_AUTOMERGE_FILE_NUM" == 0 ];then
-    echo "auto-approve" >> comments
+    echo "auto-approve" >> $PR_COMMENT_CONTENT_TMP_FILE
 else
-    echo "auto-approve is skipped as the following files are changed:" >> comments
-    { echo "\`\`\`"; git diff --name-only origin/master HEAD | grep -vE "$AUTO_MERGE_DIR_REGEX"; echo "\`\`\`"; } >> comments
+    echo "auto-approve is skipped as the following files are changed:" >> $PR_COMMENT_CONTENT_TMP_FILE
+    { echo "\`\`\`"; git diff --name-only origin/master HEAD | grep -vE "$AUTO_MERGE_DIR_REGEX"; echo "\`\`\`"; } >> $PR_COMMENT_CONTENT_TMP_FILE
 fi
+echo "auto-approve condition is: \`$AUTO_MERGE_DIR_REGEX\` defined in [](/.github/actions/check-change.sh)" >> $PR_COMMENT_CONTENT_TMP_FILE
+sed -i -z 's/\n/\\n/g' $PR_COMMENT_CONTENT_TMP_FILE
 
-echo "auto-approve condition is: \`$AUTO_MERGE_DIR_REGEX\` defined in [.github/actions/check-change.sh]()" >> comments
-
-sed -i -z 's/\n/\\n/g' comments
+export NON_AUTOMERGE_FILE_NUM=$NON_AUTOMERGE_FILE_NUM
 echo "Done"
