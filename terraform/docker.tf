22550992304ca74e50bd11ecfe541cfc4c5e046e
Naka Masato <masatonaka1989@gmail.com>
2020-07-19 13:51:40 +0900

    Terraform auto fmt & commit (#41)
    Co-authored-by: GitHub Action <action@github.com>

---
+++ terraform/docker.tf
@@ -0,0 +1,13 @@
+data "aws_ami" "example" {
+  most_recent = true
+
+  owners = ["self"]
+  tags = {
+    Name   = "app-server"
+    Tested = "true"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
