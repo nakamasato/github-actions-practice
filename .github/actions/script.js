9d7040e5b871de9eaab01cf00a85c6eb64942d32
Masato Naka <masatonaka1989@gmail.com>
2021-10-19 21:27:55 +0900

    use script

--- .github/actions/script.js
+++ .github/actions/script.js
@@ -1,17 +1,22 @@
 module.exports = ({ github, context }) => {
-    return context.payload.client_payload.value
+    const output = `#### Terraform Format and Style 🖌\`${process.env.FMT}\`
+            #### Terraform Initialization ⚙️\`${process.env.INIT}\`
+            #### Terraform Validation 🤖${process.env.VALIDATION}
+            #### Terraform Plan 📖\`${process.env.PLAN}\`
+            <details><summary>Show Plan</summary>
+
+            \`\`\`${process.env.PLAN}\`\`\`
+
+            </details>
+            *Pusher: @${context.actor}, Action: \`${context.eventName}\`, Working Directory: \`${process.env.ENV_FOR_JOB}\`, Workflow: \`${context.workflow}\`*`;
+
+    github.rest.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: output
+    })
 }
-
-
-// module.exports = ({ github, context }) => {
-//     const output = `*Pusher: @${github.actor}, Action: \`${github.event_name}\`, Working Directory: \`${env.tf_actions_working_dir}\`, Workflow: \`${github.workflow}\`*`;
-//     github.issues.createComment({
-//         issue_number: context.issue.number,
-//         owner: context.repo.owner,
-//         repo: context.repo.repo,
-//         body: output
-//     })
-// }
 
 // const output = `#### Terraform Format and Style 🖌\`${steps.fmt.outcome }\`
 // #### Terraform Initialization ⚙️\`${ steps.init.outcome }\`
