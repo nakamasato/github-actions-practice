c9b65fdb6afaa30d0ad4a16857f9b89424e5d8ab
Naka Masato <masatonaka1989@gmail.com>
2021-10-21 14:09:16 +0900

    [github-script] (#98)

---
+++ .github/actions/script.js
@@ -0,0 +1,20 @@
+module.exports = ({ github, context }) => {
+    const outputElements = [
+        `#### Terraform Format and Style 🖌\`${process.env.FMT}\``,
+        `#### Terraform Initialization ⚙️\`${process.env.INIT}\``,
+        `#### Terraform Plan 📖\`${process.env.PLAN}\``,
+        '<details><summary>Show Plan</summary>',
+        '',
+        `\`\`\`${process.env.PLAN}\`\`\``,
+        '</details>',
+        '',
+        `*Pusher: @${context.actor}, Action: \`${context.eventName}\`, Working Directory: \`${process.env.ENV_FOR_JOB}\`, Workflow: \`${context.workflow}\`*`
+    ];
+
+    github.rest.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: outputElements.join('\n')
+    })
+}
